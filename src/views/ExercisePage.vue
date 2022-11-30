df7b86e65719c13176fd800e72556fe50c4ebfd5
Desss64 <destineeekanem@gmail.com>
2022-11-29 19:38:36 -0500

    changes to pg layout

--- src/views/ExercisePage.vue
+++ src/views/ExercisePage.vue
@@ -3,49 +3,56 @@ import { useChallengeStore } from "../stores/challenge";
 
 const useChallenge = useChallengeStore();
 useChallenge.setChallenge();
-
-const completionStatus = "Start";
+useChallenge.getChallenge("How to parse markdown");
 </script>
 
 <template>
   <!-- Challenge image -->
-  <!--  src="https://placeimg.com/500/300/nature" -->
-  <q-img
-    src="https://placeimg.com/500/300/nature"
-    fit:cover
-    id="challengeImg"
-    ratio="1"
-  >
+  <!-- src="https://placeimg.com/500/300/nature" -->
+  <q-img fit:cover id="challengeImg" ratio="1">
     <h1 id="challengeTitle">{{ useChallenge.name[0] }}</h1>
   </q-img>
 
   <div class="q-pa-md row items-start q-gutter-md">
     <q-Card dark bordered id="infoBox">
-      <div id="challengeInfo">
-        <div class="description">
-          <q-card-section> {{ useChallenge.description[0] }}</q-card-section>
-
-          <!-- remove unecessary chars from challenge categories -->
+      <div>
+        <!-- Description, Time limit, and Difficulty -->
+        <div
+          class="description"
+          style="margin-top: 25px; margin-left: 25px; font-weight: bold"
+        >
+          <q-card-section>
+            {{ useChallenge.description[0] }}
+            <span
+              ><q-btn id="vmBtn"
+                >Launch VM<q-icon name="settings" style="left: 6px" /></q-btn
+            ></span>
+          </q-card-section>
           <q-card-section>
             Categories:
-            <span style="color: #2e9cca">{{
-              useChallenge.categories[0]
-            }}</span></q-card-section
-          >
+            <!-- <div
+              v-for="index in useChallenge.categories[0].length"
+              :key="index"
+            > -->
+            <span style="color: #2e9cca"
+              >{{ useChallenge.categories[0] }} &nbsp; &nbsp;</span
+            >
+            <!-- </div> -->
+          </q-card-section>
 
           <q-card-section>
-            Time Limit: {{ useChallenge.timeLimit[0] }}</q-card-section
-          >
+            Time Limit:
+            <span style="color: black">{{ useChallenge.timeLimit[0] }}</span>
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Difficulty:
+            <span style="color: black">{{ useChallenge.difficulty[0] }} </span>
+          </q-card-section>
+
           <q-card-section>
-            Difficulty: {{ useChallenge.difficulty[0] }}</q-card-section
-          >
+            <!-- create v-if here for displaying start or continue btn -->
+            <q-btn class="statusBtn">Start</q-btn>
+          </q-card-section>
         </div>
       </div>
-
-      <!-- change color of text based on difficulty -->
-
-      <!-- create v-if here for displaying start or continue btn -->
-      <q-btn style="background-color: green">{{ completionStatus }}</q-btn>
     </q-Card>
   </div>
 </template>
@@ -57,6 +64,7 @@ const completionStatus = "Start";
   margin-left: 260px;
   margin-top: 140px;
   font-size: 40px;
+  font-weight: bold;
 }
 
 #infoBox {
@@ -64,20 +72,30 @@ const completionStatus = "Start";
   height: 680px;
   margin-left: 250px;
   margin-right: 150px;
-  margin-top: 35px;
+  margin-top: 20px;
   background-color: #464866;
   border-radius: 10px;
 }
 
-#challengeInfo {
-  text-align: left;
-}
-
 #challengeImg {
-  background-color: #464866;
+  background-color: #29648a;
   margin-bottom: 20px;
   max-width: 8000px;
   height: 240px;
   margin-top: -25px;
+}
+
+.statusBtn {
+  background-color: green;
+  color: white;
+}
+
+#vmBtn {
+  background-color: #797373;
+  color: black;
+  font-weight: bold;
+  font-size: 13px;
+  width: 145px;
+  left: 555px;
 }
 </style>
